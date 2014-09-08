5bc9ed7bc6366b113c242d3dc8bc9e0458b106d3
bgnori <bgnori@gmail.com>
2014-09-08 11:31:38 +0900

    gp fmt

--- tarai.go
+++ tarai.go
@@ -1,21 +1,20 @@
 package main
 
 import (
+	"./broom"
 	"fmt"
-        "./broom"
-        "strings"
+	"strings"
 )
 
 const tarai = "(define tarai (lambda (x y z) (if (> x y) (tarai (tarai (- x 1) y z) (tarai (- y 1) z x) (tarai (- z 1) x y)) y))) (tarai 3 2 1)"
 
 func main() {
 	env := broom.NewGlobalRootFrame()
-        buf := broom.NewBuffered(strings.NewReader(tarai))
+	buf := broom.NewBuffered(strings.NewReader(tarai))
 
-        for i, expr := range broom.BuildSExpr(buf) {
-          fmt.Println("input[", i, "]:", expr)
-          got := broom.Eval(expr, env)
-          fmt.Println("-->", got)
-        }
+	for i, expr := range broom.BuildSExpr(buf) {
+		fmt.Println("input[", i, "]:", expr)
+		got := broom.Eval(expr, env)
+		fmt.Println("-->", got)
+	}
 }
-
