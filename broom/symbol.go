9e83d0b234863365cf843c84eb69894d23442c83
bgnori <bgnori@gmail.com>
2014-10-02 18:51:48 +0900

    id based interned symbol. 	modified:   broom/symbol.go

--- broom/symbol.go
+++ broom/symbol.go
@@ -1,25 +1,86 @@
 package broom
 
+import (
+	"fmt"
+	"sync"
+)
+
 type symbolImpl struct {
-	value string
+	id int
 }
 
-func sym(s string) *symbolImpl {
-	return &symbolImpl{value: s}
+type Pool struct {
+	mutex sync.RWMutex
+	lookup map[string] int
+	xs []string
+}
+
+var pool *Pool
+
+func initPool() {
+	pool = &Pool{lookup:make(map[string]int), xs: make([]string, 0)}
+}
+
+func (p* Pool)Lock() { p.mutex.Lock() }
+func (p* Pool)Unlock() { p.mutex.Unlock() }
+func (p* Pool)RLock() { p.mutex.RLock() }
+func (p* Pool)RUnlock() { p.mutex.RUnlock() }
+
+func (p* Pool)MakeSymbol(s string) *symbolImpl{
+	p.xs = append(p.xs, s)
+	n := len(p.xs) - 1
+	p.lookup[s] = n
+	return &symbolImpl{id:n}
+}
+
+func (p* Pool)LookUp(s string) (*symbolImpl, bool) {
+	if n, ok := p.lookup[s]; ok {
+		return &symbolImpl{id:n}, true
+	}
+	return nil, false
+}
+
+func (p* Pool)GetValue(n int) string {
+	return p.xs[n]
+}
+
+func sym(t string) *symbolImpl {
+	if pool == nil {
+		initPool()
+	}
+	pool.RLock()
+	if s, ok := pool.LookUp(t); ok {
+		pool.RUnlock()
+		return s
+	}
+	pool.RUnlock()
+
+	pool.Lock()
+	defer pool.Unlock()
+	if s, ok := pool.LookUp(t); ok {
+		return s
+	}
+	return pool.MakeSymbol(t)
 }
 
 func (s *symbolImpl) GetValue() string {
-	return s.value
+	pool.RLock()
+	defer pool.RUnlock()
+	return pool.GetValue(s.id)
 }
 
 func (s *symbolImpl) Eq(other interface{}) bool {
 	if t, ok := other.(*symbolImpl); ok {
-		return s.value == t.value
+		return s.id == t.id
 	} else {
 		return false
 	}
 }
 
 func (s *symbolImpl) String() string {
-	return s.value
+	return s.GetValue()
+}
+
+func (s *symbolImpl) DetailedPrint() string {
+	return fmt.Sprintf("#symbol-%d-:%s", s.id, s.GetValue())
 }
